e298544f9f77fa96d7f802447cd9fb469d2b05c0
Han Liang Wee Eric <Eric.aka.darth_vader@hotmail.com>
2018-05-19 01:55:15 +0800

    Removed other Barcode

--- datamatrix/datamatrixcode.go
+++ datamatrix/datamatrixcode.go
@@ -4,8 +4,8 @@ import (
 	"image"
 	"image/color"
 
-	"github.com/boombuler/barcode"
-	"github.com/boombuler/barcode/utils"
+	"github.com/eric-vader/barcode-go"
+	"github.com/eric-vader/barcode-go/utils"
 )
 
 type datamatrixCode struct {
